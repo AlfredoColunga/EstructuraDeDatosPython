1ecbc15ca01ae892ccb054d622b933a25861c2cf
AlfredoColunga <60327428+AlfredoColunga@users.noreply.github.com>
2024-06-06 13:18:46 -0600

    Delete crearTablaSQL.py

--- crearTablaSQL.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-from sqlite3 import Error
-
-def sql_connection():
-    try:
-        con = sqlite3.connect('(archivo db')
-        return con
-    except Error:
-        print(Error)
